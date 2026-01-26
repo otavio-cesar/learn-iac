116fc83090b94ba78b03d6300a3c32f69d910aa1
otavio <otavio.oliveira@avitaseg.com.br>
2026-01-26 15:45:17 -0300

    commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+   
+   assume_role {
+    role_arn     = "arn:aws:iam::045444243386:role/terra"
+  }
+}
+
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+
+#   filter {
+#     name = "name"
+#     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+#   }
+
+#   owners = ["099720109477"] # Canonical
+# }
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t3.micro"
+  key_name = "par-chaves"
+  # user_data = <<-EOF
+  #                 #!/bin/bash
+  #                       cd /home/ubuntu
+  #                       echo "<h1>Feito com Terraform</h1>" > index.html
+  #                       nohup busybox httpd -f -p 8080 &
+  #                 EOF
+  tags = {
+    Name = "learn-terraform"
+  }
+}
+
+
